1d8ed4663015dbe5cf658441ce022cb2f502c634
Ralu <ralu2K3@gmail.com>
2022-03-24 23:24:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { clear } from "@testing-library/user-event/dist/clear";
+import { useState } from "react";
 
-function App() {
+
+const App = () => {
+  const [value, setValue] = useState("") 
+
+const display = (e) => {
+  setValue(value?.concat(e.target?.value))
+}
+const clear = () => {
+  setValue("")
+}
+const backSpace = () => {
+  setValue(value?.slice(0, -1))
+  
+}
+
+const logic = () => {
+  try {
+    setValue(Number(eval(value).toString()).toFixed(2))
+  } catch (error) { 
+    setValue("Invalid Format")
+    
+  }
+
+
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="calc">
+    <input type="text" name="input" size="16" id="answer" value={value}/>
+    <div class="buttons">
+        <button onClick = {clear} class='operator' id='c'>C</button>
+        <button value = "%" onClick = {display} class='operator'>%</button>
+        <button class='operator' onClick = {backSpace}>&larr;</button>
+        <button value = "/" onClick = {display} class='operator'>/</button>
+        <button value = "1" onClick = {display}>1</button>
+        <button value = "2" onClick = {display}>2</button>
+        <button value = "3" onClick = {display}>3</button>
+        <button value = "*" onClick = {display} class='operator'>*</button>
+        <button value = "4" onClick = {display}>4</button>
+        <button value = "5" onClick = {display}>5</button>
+        <button value = "6" onClick = {display}>6</button>
+        <button value = "-" onClick = {display} class='operator'>-</button>
+        <button value = "7" onClick = {display}>7</button>
+        <button value = "8" onClick = {display}>8</button>
+        <button value = "9" onClick = {display}>9</button>
+        <button value = "+" onClick = {display} class='operator'>+</button>
+        <button value = "." onClick = {display}>.</button>
+        <button value = "0" onClick = {display}>0</button>
+        <button value = "" onClick = {display}></button>
+        <button class='operator' onClick= {logic}>=</button>
+
     </div>
+
+
+</div>
   );
 }
 
